8d566a49942c8c1f8ecceaec362473310af36d9e
Em1L777 <emilfaizullov@gmail.com>
2026-03-03 11:10:13 +0100

    chore: connected components(hero,company,contacts,product) globally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './components/hero/hero.component';
+import { CompanyComponent } from './components/company/company.component';
+import { ProductComponent } from './components/product/product.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [HeroComponent, CompanyComponent, ProductComponent, ContactsComponent, RouterOutlet, NavbarComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
