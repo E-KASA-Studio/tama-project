2cae2263e986495db39ca4982a8d1c1f5c0d85fd
Em1L777 <emilfaizullov@gmail.com>
2026-03-03 10:33:59 +0100

    chore: modified app.component.ts, connecting NavbarComponent globaly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'tama-project';
 }
