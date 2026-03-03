16badb970ca2079cbb16d3cf52a0c2fc193ca121
Em1L777 <emilfaizullov@gmail.com>
2026-03-03 13:48:35 +0100

    chore: delete components from app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { HeroComponent } from './components/hero/hero.component';
-import { CompanyComponent } from './components/company/company.component';
-import { ProductComponent } from './components/product/product.component';
-import { ContactsComponent } from './components/contacts/contacts.component';
+
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeroComponent, CompanyComponent, ProductComponent, ContactsComponent, RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
