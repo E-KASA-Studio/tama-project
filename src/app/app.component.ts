c855b344af452d4a9e99b62426828c470b2a7d88
Em1L777 <emilfaizullov@gmail.com>
2026-03-09 13:36:21 +0100

    feat: import HeroComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component, inject } from '@angular/core';
 import { HeroComponent } from './features/sections/hero/hero.component';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+<<<<<<< HEAD
 import { TranslateService, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavbarComponent, TranslatePipe],
+=======
+import { HeroComponent } from "./features/sections/hero/hero.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NavbarComponent, HeroComponent],
+>>>>>>> 162abd0 (feat: import HeroComponent)
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
