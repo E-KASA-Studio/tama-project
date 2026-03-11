f3cdb9416d28bfaaf54ab8a6127fefa8161dd1ae
Ilmira Dozhdikova <dozhdikovailmira@gmail.com>
2026-03-11 16:52:23 +0100

    test(i18n): add integration tests for language.service

--- src/app/core/services/language.service.spec.ts
+++ src/app/core/services/language.service.spec.ts
@@ -1,16 +1,61 @@
 import { TestBed } from '@angular/core/testing';
-
 import { LanguageService } from './language.service';
+import { TranslateService, provideTranslateService } from '@ngx-translate/core';
+import { LanguageCode } from './models/language.model';
+import { PLATFORM_ID } from '@angular/core';
+import { of } from 'rxjs';
 
 describe('LanguageService', () => {
-  let service: LanguageService;
+  let translate: TranslateService;
+  const STORAGE_KEY = 'user_lang';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LanguageService);
+    localStorage.clear();
+
+    TestBed.configureTestingModule({
+      providers: [
+        provideTranslateService(),
+        LanguageService,
+        { provide: PLATFORM_ID, useValue: 'browser' }
+      ]
+    });
+
+    translate = TestBed.inject(TranslateService);
+    spyOn(translate, 'use').and.returnValue(of({}));
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should initialize with default language (RU) when storage is empty', () => {
+
+    const service = TestBed.inject(LanguageService);
+    
+    expect(service.currentLanguage()).toBe(LanguageCode.RU);
+    expect(translate.use).toHaveBeenCalledWith(LanguageCode.RU);
+  });
+
+  it('should switch language correctly and save to localStorage', () => {
+    const service = TestBed.inject(LanguageService);
+    
+    service.switchLanguage(LanguageCode.EN);
+
+    expect(service.currentLanguage()).toBe(LanguageCode.EN);
+    expect(localStorage.getItem(STORAGE_KEY)).toBe(LanguageCode.EN);
+    expect(translate.use).toHaveBeenCalledWith(LanguageCode.EN);
+  });
+
+  it('should read language from localStorage on startup', () => {
+    localStorage.setItem(STORAGE_KEY, LanguageCode.EN);
+
+    const service = TestBed.inject(LanguageService);
+
+    expect(service.currentLanguage()).toBe(LanguageCode.EN);
+    expect(translate.use).toHaveBeenCalledWith(LanguageCode.EN);
+  });
+
+  it('should fallback to RU if localStorage has invalid value', () => {
+    localStorage.setItem(STORAGE_KEY, 'invalid_lang' as any);
+    
+    const service = TestBed.inject(LanguageService);
+
+    expect(service.currentLanguage()).toBe(LanguageCode.RU);
   });
 });
