491bbff568d099d08611f1ae670f512ef56dcc20
Em1L777 <emilfaizullov@gmail.com>
2026-03-12 10:25:14 +0100

    feat: update after rebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,10 @@ import { Component, inject } from '@angular/core';
 import { HeroComponent } from './features/sections/hero/hero.component';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './layout/navbar/navbar.component';
-<<<<<<< HEAD
-import { HeroComponent } from "./features/sections/hero/hero.component";
-import { TranslateService, TranslatePipe } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, TranslatePipe],
-=======
-import { HeroComponent } from "./features/sections/hero/hero.component";
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HeroComponent],
->>>>>>> 162abd0 (feat: import HeroComponent)
   imports: [RouterOutlet, NavbarComponent, HeroComponent],
   standalone: true,
   templateUrl: './app.component.html',
@@ -24,7 +14,5 @@ import { HeroComponent } from "./features/sections/hero/hero.component";
 
 export class AppComponent {
   translate = inject(TranslateService);
-
-
   title = 'tama-project';
 }
