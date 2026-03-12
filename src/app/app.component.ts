e1175283a12a676c0744957444ee57d27d49ab8a
Em1L777 <emilfaizullov@gmail.com>
2026-03-12 11:09:51 +0100

    feat: update hero section, add responsive to bg and text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component, inject } from '@angular/core';
 import { HeroComponent } from './features/sections/hero/hero.component';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './layout/navbar/navbar.component';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateService, TranslatePipe  } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HeroComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, TranslatePipe ],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
@@ -14,5 +14,9 @@ import { TranslateService } from '@ngx-translate/core';
 
 export class AppComponent {
   translate = inject(TranslateService);
+  constructor() {
+    this.translate.setDefaultLang('ru');
+    this.translate.use('ru');
+  }
   title = 'tama-project';
 }
