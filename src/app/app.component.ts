37c4cbef660d8b08edda302b09081e6897a5732f
Sergey Dozhdikov <sergey.dozhdikov@students.salle.url.edu>
2026-03-13 09:24:51 +0900

    Move shell rendering to app component and 'tailwindize' navbar states

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { TranslateService, TranslatePipe } from '@ngx-translate/core';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { LandingPageComponent } from './features/landing-page/landing-page.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TranslatePipe],
+  imports: [TranslatePipe, NavbarComponent, LandingPageComponent, FooterComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { LandingPageComponent } from '../features/landing-page/landing-page.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [NavbarComponent, FooterComponent, LandingPageComponent],
+  standalone: true,
+  imports: [FooterComponent, LandingPageComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
