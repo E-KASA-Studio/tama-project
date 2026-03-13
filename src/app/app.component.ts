3950612bfbc660145532e34f7031223fd60a46ef
Em1L777 <emilfaizullov@gmail.com>
2026-03-14 00:23:02 +0100

    feat: connect product component to app component and add background image to product section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { HeroComponent } from './features/sections/hero/hero.component';
-import { NavbarComponent } from './layout/navbar/navbar.component';
+import { ProductComponent } from './features/sections/product/product.component';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 import { TranslateService, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HeroComponent, TranslatePipe ],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, ProductComponent, TranslatePipe ],
 
   standalone: true,
   templateUrl: './app.component.html',
