c3ced3860489de55d740e5dcbf81762005367a89
Ilmira Dozhdikova <dozhdikovailmira@gmail.com>
2026-03-14 15:35:38 +0100

    feat(contacts):import translatePipe

--- src/app/features/sections/contacts/contacts.component.ts
+++ src/app/features/sections/contacts/contacts.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contacts',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
