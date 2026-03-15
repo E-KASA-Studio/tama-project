31957feade702d6838d57ea86a0dd9a7e9a7d4db
Em1L777 <emilfaizullov@gmail.com>
2026-03-15 12:23:52 +0100

    feat: connect custom elements schema

--- src/app/features/sections/product/product.component.ts
+++ src/app/features/sections/product/product.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-product',
   imports: [],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrl: './product.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ProductComponent {
 
