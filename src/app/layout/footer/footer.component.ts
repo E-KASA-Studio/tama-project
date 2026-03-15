acb842b105d8c35a4a0b5edd818a52049b0f4b1d
Sergey Dozhdikov <sergey.dozhdikov@students.salle.url.edu>
2026-03-15 19:59:33 +0900

    Removed out of scope changes

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -6,23 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
-  navCompany = 'Company';
-  navProduct = 'Product';
-  navContacts = 'Contacts';
-
-  company = 'TAMA CAPS, Ltd';
-  address = '113925, City, Street, 00';
-  email = 'test@tamacaps.kz';
-  phone = '+XX XXX XX XX XX';
-
-  scrollToSection(sectionId: string): void {
-    const section = document.getElementById(sectionId);
-    if (!section) {
-      return;
-    }
-
-    const top = section.getBoundingClientRect().top + window.scrollY - 96;
-    window.scrollTo({ top, behavior: 'smooth' });
-  }
-}
+export class FooterComponent {}
