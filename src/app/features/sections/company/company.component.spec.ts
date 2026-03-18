57c34a5f791cf3b1d1e8c1060d1fb213b0c55a9f
Em1L777 <emilfaizullov@gmail.com>
2026-03-18 12:19:43 +0100

    feat: edit test file

--- src/app/features/sections/company/company.component.spec.ts
+++ src/app/features/sections/company/company.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { CompanyComponent } from './company.component';
 
 describe('CompanyComponent', () => {
@@ -8,7 +8,7 @@ describe('CompanyComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CompanyComponent]
+      imports: [CompanyComponent, TranslateModule.forRoot()]
     })
     .compileComponents();
 
