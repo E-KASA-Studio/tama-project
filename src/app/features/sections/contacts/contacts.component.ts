5ad0523eb7b3c76f56c10157114a9da6746d1323
Ilmira Dozhdikova <dozhdikovailmira@gmail.com>
2026-03-18 16:22:12 +0100

    feat:add logic for computed signals for contact links

--- src/app/features/sections/contacts/contacts.component.ts
+++ src/app/features/sections/contacts/contacts.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { LanguageService } from '../../../core/services/language.service';
 
 @Component({
   selector: 'app-contacts',
@@ -8,5 +9,21 @@ import { TranslatePipe } from '@ngx-translate/core';
   styleUrl: './contacts.component.css'
 })
 export class ContactsComponent {
+  private readonly languageService = inject(LanguageService);
+
+  private readonly emailKey = this.languageService.translationSignal('contactsSection.email');
+  private readonly phoneKey = this.languageService.translationSignal('contactsSection.phone');
+  private readonly addressKey = this.languageService.translationSignal('contactsSection.address');
+
+readonly emailHref = computed(() => `mailto:${this.emailKey()}`);
+
+readonly phoneHref = computed(() => 
+  `tel:${this.phoneKey().replace(/\s/g, '')}`
+);
+
+readonly mapsHref = computed(() => 
+  `https://maps.google.com/?q=${encodeURIComponent(this.addressKey())}`
+);
+
 
 }
