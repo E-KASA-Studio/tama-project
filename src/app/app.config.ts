aaadb172dec1c05d01c6510193d94105fe8f494d
Em1L777 <emilfaizullov@gmail.com>
2026-03-19 19:26:12 +0100

    feat: made smooth scroll in app config ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import {provideHttpClient} from "@angular/common/http";
 
 import { routes } from './app.routes';
@@ -9,7 +9,9 @@ import {provideTranslateHttpLoader} from "@ngx-translate/http-loader";
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withInMemoryScrolling({anchorScrolling: 'enabled',
+    scrollPositionRestoration: 'enabled'
+  })),
     provideHttpClient(),
     provideTranslateService({
       loader: provideTranslateHttpLoader({
