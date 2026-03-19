ac804224c1e5f830e8a641458a4cabdc3e454e06
Em1L777 <emilfaizullov@gmail.com>
2026-03-19 19:13:55 +0100

    feat: create core navigation structure

--- src/app/features/services/landing-scroll.service.ts
+++ src/app/features/services/landing-scroll.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LandingScrollService {
 
-  constructor() { }
+  constructor(private viewportScroller: ViewportScroller) {
+    // Настройка смещения (например, если у тебя фиксированный navbar высотой 64px)
+    this.viewportScroller.setOffset([0, 64]);
+  }
+
+  /**
+   * Скролл к элементу по ID
+   * @param elementId ID целевого элемента
+   */
+  scrollToSection(elementId: string): void {
+    this.viewportScroller.scrollToAnchor(elementId);
+  }
 }
