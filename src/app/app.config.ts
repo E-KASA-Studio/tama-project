ad64a58d1d208531deac69e76701837f5d8111ad
Em1L777 <emilfaizullov@gmail.com>
2026-03-20 12:00:05 +0100

    feat: changed app config ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import {provideHttpClient} from "@angular/common/http";
 
 import { routes } from './app.routes';
@@ -9,9 +9,7 @@ import {provideTranslateHttpLoader} from "@ngx-translate/http-loader";
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withInMemoryScrolling({anchorScrolling: 'enabled',
-    scrollPositionRestoration: 'enabled'
-  })),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
     provideHttpClient(),
     provideTranslateService({
       loader: provideTranslateHttpLoader({
