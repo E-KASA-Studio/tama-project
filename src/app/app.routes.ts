3c8ae0452f49f8ec61ba5dfda4fb81703f5d0cae
Ilmira Dozhdikova <dozhdikovailmira@gmail.com>
2026-03-20 11:57:57 +0100

    feat:setup anchorScrolling config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import { RouterFeatures, Routes, withInMemoryScrolling } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
@@ -6,4 +6,11 @@ export const routes: Routes = [
 		path: '',
 		component: LayoutComponent
 	}
+];
+
+export const routerOptions: RouterFeatures[] = [
+  withInMemoryScrolling({
+    anchorScrolling: 'enabled',        
+    scrollPositionRestoration: 'enabled',
+  })
 ];
