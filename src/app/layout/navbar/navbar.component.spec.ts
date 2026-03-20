6589417bf4fa9950b11e1477ad3547e37eee5347
Ilmira Dozhdikova <dozhdikovailmira@gmail.com>
2026-03-20 12:40:39 +0100

    fix:import provideRouter for tests

--- src/app/layout/navbar/navbar.component.spec.ts
+++ src/app/layout/navbar/navbar.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavbarComponent } from './navbar.component';
 import { provideTranslateService } from '@ngx-translate/core';
+import { provideRouter } from '@angular/router';
 
 
 describe('NavbarComponent', () => {
@@ -10,7 +11,7 @@ describe('NavbarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NavbarComponent],
-      providers: [provideTranslateService()]
+      providers: [provideTranslateService(), provideRouter([])]
     })
     .compileComponents();
 
