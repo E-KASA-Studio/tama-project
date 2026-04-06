7cd2070fb362cc30f38b335ecce2e849f1301860
Sergey Dozhdikov <sergey.dozhdikov@students.salle.url.edu>
2026-04-06 20:59:27 +0900

    feat(ssg): enable Angular prerender with client hydration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 import { Meta } from '@angular/platform-browser';
 import { environment } from '../environments/environment';
 
@@ -15,11 +16,13 @@ import { environment } from '../environments/environment';
 })
 export class AppComponent implements OnInit{
   title = 'tama-project';
+  private readonly platformId = inject(PLATFORM_ID);
+  private readonly isBrowser = isPlatformBrowser(this.platformId);
 
   constructor(private meta: Meta) {}
 
   ngOnInit(): void {
-    if ('scrollRestoration' in window.history) {
+    if (this.isBrowser && 'scrollRestoration' in window.history) {
       window.history.scrollRestoration = 'manual';
     }
 
