6a3f91ae5fc4168c65035f63c7a5d58826b388da
Sergey Dozhdikov <sergey.dozhdikov@students.salle.url.edu>
2026-04-12 07:08:21 +0900

    feat(ssg): bake RU translations into prerendered HTML

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,13 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
+import { provideTranslateLoader } from '@ngx-translate/core';
 import { appConfig } from './app.config';
+import { RuPrerenderLoader } from './core/i18n/ru-prerender.loader';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
+    provideTranslateLoader(RuPrerenderLoader),
   ]
 };
 
