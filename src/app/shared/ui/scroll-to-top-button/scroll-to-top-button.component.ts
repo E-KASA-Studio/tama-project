47f96e4c221f6f87e266aad3013da6ffa5aaa82d
Em1L777 <emilfaizullov@gmail.com>
2026-04-23 21:23:42 +0200

    feat: added logic for go up button

--- src/app/shared/ui/scroll-to-top-button/scroll-to-top-button.component.ts
+++ src/app/shared/ui/scroll-to-top-button/scroll-to-top-button.component.ts
@@ -1,9 +1,11 @@
 import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { Component, HostListener, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Импортируем для работы директивы *ngIf
 
 @Component({
   selector: 'app-scroll-to-top-button',
-  imports: [],
+  standalone: true, // Убедитесь, что компонент standalone, если используете Angular 17+
+  imports: [CommonModule],
   templateUrl: './scroll-to-top-button.component.html',
   styleUrl: './scroll-to-top-button.component.css'
 })
@@ -12,16 +14,37 @@ export class ScrollToTopButtonComponent implements OnInit {
   private readonly platformId = inject(PLATFORM_ID);
   private readonly isBrowser = isPlatformBrowser(this.platformId);
 
+  // Переменная для контроля видимости
+  isVisible: boolean = false;
+
+  // Слушаем прокрутку окна
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Показываем кнопку, если прокрутили больше чем на 300px (высота "первого экрана")
+    const verticalOffset = window.pageYOffset 
+      || document.documentElement.scrollTop 
+      || document.body.scrollTop || 0;
+      this.updateBottomOffset();
+
+    this.isVisible = verticalOffset > 300;
+  }
+
+  // Метод для плавной прокрутки вверх
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
+
   bottomOffsetPx = 16;
 
   ngOnInit(): void {
     this.updateBottomOffset();
   }
 
-  @HostListener('window:scroll')
-  onWindowScroll(): void {
-    this.updateBottomOffset();
-  }
+
 
   @HostListener('window:resize')
   onWindowResize(): void {
