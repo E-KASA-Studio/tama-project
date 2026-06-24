3292a8931e57e33c0583f612580271585f769da1
Em1L777 <emilfaizullov@gmail.com>
2026-06-24 20:33:31 +0200

    feat: add cookie banner and privacy policy page

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,11 +3,13 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { LandingPageComponent } from '../features/landing-page/landing-page.component';
 import { ScrollToTopButtonComponent } from '../shared/ui/scroll-to-top-button/scroll-to-top-button.component';
+import { CookieBannerComponent } from '../shared/cookie-banner/cookie-banner.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, LandingPageComponent, ScrollToTopButtonComponent],
+  imports: [NavbarComponent, FooterComponent, LandingPageComponent, ScrollToTopButtonComponent, CookieBannerComponent, RouterModule],
   templateUrl: './layout.component.html'
 })
 export class LayoutComponent {
